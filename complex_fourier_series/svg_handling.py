68ae987ed81c7a602ae94a9cd6e3224f8821cbec
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-02-07 00:48:11 +0300

    Rename fourier_series folder to complex_fourier_series, rename PATH_FUNCTION to FLOAT_TO_COMPLEX

--- complex_fourier_series/svg_handling.py
+++ complex_fourier_series/svg_handling.py
@@ -11,7 +11,7 @@ __all__ = (
     "Segments",
     "DEFAULT_SCALAR",
     "DEFAULT_BIAS",
-    "PATH_FUNCTION",
+    "FLOAT_TO_COMPLEX",
     "get_svg_path_elements",
     "parse_paths",
     "create_path_function",
@@ -23,7 +23,7 @@ Segments = Union[
 
 DEFAULT_SCALAR: complex = 1 + 0j
 DEFAULT_BIAS: complex = 0 + 0j
-PATH_FUNCTION = Callable[[float], complex]
+FLOAT_TO_COMPLEX = Callable[[float], complex]
 
 
 def get_svg_path_elements(filename: str) -> List[str]:
@@ -57,7 +57,7 @@ def parse_paths(path_elements: List[str]) -> Path:
 
 def create_path_function(
         path: Path, scalar: complex = DEFAULT_SCALAR,
-        bias: complex = DEFAULT_BIAS) -> PATH_FUNCTION:
+        bias: complex = DEFAULT_BIAS) -> FLOAT_TO_COMPLEX:
 
     def f(t: float) -> complex:
         return scalar * path.point(t) + bias
@@ -67,9 +67,36 @@ def create_path_function(
 
 def create_path_function_from_file(
         filename: str, scalar: complex = DEFAULT_SCALAR,
-        bias: complex = DEFAULT_BIAS) -> PATH_FUNCTION:
+        bias: complex = DEFAULT_BIAS) -> FLOAT_TO_COMPLEX:
 
     return create_path_function(
         parse_paths(get_svg_path_elements(name)),
         scalar, bias)
 
+
+if __name__ == "__main__":
+    n = 0.5
+    scale = 8
+    x_off = 0  # pd.scr[0]/16
+    y_off = 0  # pd.scr[1]/10
+    name = "./icons/" + "pi.svg"
+
+    f = create_path_function_from_file(name)
+
+    # , n, scale=scale, x_off=x_off, y_off=y_off, cent=1)
+
+    #_ = pd.pau()
+    # for x,y in path:
+    #     pd.circ("white", (x+wid/2, y+hei/2), 2)
+    #     pd.upd()
+    # _ = pd.pau()
+
+    # for item in segments:
+    #     length = item.length()
+    #     # lengths.append(length := item.length())
+    #     pass
+    # step = round(length/freq)
+    # for time in range(step):
+    #     time/=step
+    #     pos = item.point(time)
+    #     path.append([(pos.real+x_off)*scale, (pos.imag+y_off)*scale])
