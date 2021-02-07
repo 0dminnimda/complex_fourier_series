f092b5de72a732bcba674ac6c35954f6937d46ce
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-02-07 21:11:26 +0300

    Update svg_handling.py: delete `if __name__`

--- complex_fourier_series/svg_handling.py
+++ complex_fourier_series/svg_handling.py
@@ -72,31 +72,3 @@ def create_path_function_from_file(
     return create_path_function(
         parse_paths(get_svg_path_elements(filename)),
         scalar, bias)
-
-
-if __name__ == "__main__":
-    n = 0.5
-    scale = 8
-    x_off = 0  # pd.scr[0]/16
-    y_off = 0  # pd.scr[1]/10
-    name = "./icons/" + "pi.svg"
-
-    f = create_path_function_from_file(name)
-
-    # , n, scale=scale, x_off=x_off, y_off=y_off, cent=1)
-
-    #_ = pd.pau()
-    # for x,y in path:
-    #     pd.circ("white", (x+wid/2, y+hei/2), 2)
-    #     pd.upd()
-    # _ = pd.pau()
-
-    # for item in segments:
-    #     length = item.length()
-    #     # lengths.append(length := item.length())
-    #     pass
-    # step = round(length/freq)
-    # for time in range(step):
-    #     time/=step
-    #     pos = item.point(time)
-    #     path.append([(pos.real+x_off)*scale, (pos.imag+y_off)*scale])
