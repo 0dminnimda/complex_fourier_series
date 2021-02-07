186257ad3b1fef3494d6aae3fcbc532941dfb112
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-02-07 22:49:46 +0300

    Update __init__.py

--- complex_fourier_series/__init__.py
+++ complex_fourier_series/__init__.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 
-__all__ = ()
+from .animator import main_loop
+from .svg_handling import create_path_function_from_file
+
+__all__ = (
+    "main_loop",
+    "create_path_function_from_file",
+    "__name__",
+    "__author__",
+    "__version__")
 
 
 __name__ = "complex_fourier_series"
 __author__ = "0dminnimda"
+__version__ = "0.1.0"
