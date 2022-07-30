70d5a91281e053c8f819c94d529777c155f364fa
0dminnimda <0dminnimda@gmail.com>
2022-07-30 23:40:15 +0300

    manual_test.py: add the new icons

--- test/manual_test.py
+++ test/manual_test.py
@@ -1,19 +1,35 @@
 from complex_fourier_series import main_loop, create_path_function_from_file
 
-name = "fourier"
+name = "pi"
 
 vals = {
-    "pi": {"scalar": 5, "offset": - 50 - 50j},
-    "square": {"scalar": 25, "offset": - 10 - 10j},
-    "fourier": {"scalar": 0.5, "offset": - 130 + 130j},
+    # (scalar, offset)
+    "Autonomous_oblast_of_Russia": (),
+    "axe": (),
+    "box": (),
+    "chick": (),
+    "curve": (),
+    "facebook": (),
+    "flower": (),
+    "fourier": (0.5, -130 + 130j),
+    "func": (),
+    "github": (),
+    "hors": (),
+    "loylo": (),
+    "mnote": (),
+    "pi": (5, -50 - 50j),
+    "python-b": (),
+    "python": (),
+    "sear": (),
+    "square": (25, -10 - 10j),
+    "sum": (),
+    "youtube": (),
 }
 
-val = vals[name]
-
-name = "./icons/" + name + ".svg"
-
+quantity = 10
 main_loop(
     (900, 700),
-    create_path_function_from_file(name, **val),
-    15,
-    5000)
+    create_path_function_from_file(f"./icons/{name}.svg", *vals[name]),
+    quantity,
+    5000,
+)
