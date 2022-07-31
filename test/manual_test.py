d2088e3788432d6c5755a0911e3242a63b6885ad
0dminnimda <0dminnimda@gmail.com>
2022-07-31 16:34:53 +0300

    manual_test.py: enhance the animation

--- test/manual_test.py
+++ test/manual_test.py
@@ -34,12 +34,21 @@ vals = {
 #     5000,
 # )
 
-for quantity in range(1, 50):
+length = 500
+max_quantity = 50
+per_increase = length // max_quantity
+
+for quantity in range(1, max_quantity + 1):
+    print(quantity, end="\r")
+    start = (quantity - 1) * per_increase
     if main_loop(
         (900, 700),
         create_path_function_from_file(f"./icons/{name}.svg", *vals[name]),
         quantity,
-        500,
-        save="animation"
+        length,
+        save="animation/mixed",
+        save_from=length + start,
+        time_start=0,
+        time_end=length + start + per_increase,
     ):
         break
